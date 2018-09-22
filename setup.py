44210812268cb3dbbaaee8caa58e48e4c47372e9
Lawrence Wong <lcwong@mit.edu>
2018-09-22 11:50:30 -0400

    Query is Fixed
    Signed-off-by: Lawrence Wong <lcwong@mit.edu>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyNA',
+    version='1.0.0',
+    packages=['python', 'python.gtf', 'python.gtf.visual', 'python.vcf', 'python.mutation'],
+    url='https://github.com/lcwong0928/PyNA/',
+    license='MIT License',
+    author='Lawrence C. Wong',
+    author_email='lcwong@mit.edu',
+    description='RNAseq Computation Pipeline'
+)
